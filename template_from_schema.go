faf97c1ab536f816332a53164d9c98675fce2206
Sascha Andres <sascha.andres@outlook.com>
2025-06-09 10:22:50 +0200

    feat(UI): port document from schema to new UI

---
+++ template_from_schema.go
@@ -0,0 +1,12 @@
+package jsonedit
+
+const fromSchemaFormTemplate = `
+<form action="/from-schema" method="post" enctype="multipart/form-data" id="form_from_schema" hx-encoding="multipart/form-data">
+	<h2>Generate JSON from Schema</h2>
+	<div>
+		<label for="schemaFile">JSON Schema File:</label>
+		<input type="file" name="schemaFile" accept=".json" required>
+	</div>
+	<button form="form_from_schema" type="submit" hx-redirect="/from-schema">Compare</button>
+</form>
+`
